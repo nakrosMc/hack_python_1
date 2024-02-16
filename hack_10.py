e7a03cdd064bceff90d671a2ebdfe5f709051de8
Nakros <nakrosnc@gmail.com>
2024-02-16 02:04:41 +0100

    a

--- hack_10.py
+++ hack_10.py
@@ -4,8 +4,18 @@ text: "fooziman" output => ["F","0","0","Z","1","M","@","N"]
 
 def fn_hack_10():
     result = "fooziman"
-    result = ["F","0","0","Z","1","M","@","N"]
-    return result 
-
+    new_result = []
+    for txt in result:
+        if txt == 'o':
+            new_result.append('0')
+        elif txt == 'i':
+            new_result.append('1')
+        elif txt == 'a':
+            new_result.append('@')
+        else:
+            new_result.append(txt.upper())
+             
+    return new_result
+            
 hack_10 = fn_hack_10()
 print(hack_10)
