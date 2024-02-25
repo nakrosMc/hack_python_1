615efd59b9eaeb0bc31990d9c5349c0f5d42999b
nakrosMc <nakrosnc@gmail.com>
2024-02-24 20:15:10 -0400

    a

--- hack_10.py
+++ hack_10.py
@@ -4,8 +4,9 @@ text: "fooziman" output => ["F","0","0","Z","1","M","@","N"]
 
 def fn_hack_10():
     result = "fooziman"
-    result = ["F","0","0","Z","1","M","@","N"]
-    return result 
+    new_result = result.replace("fooziman","F00Z1M@N")
+    ls = list(new_result)
+    return ls
 
 hack_10 = fn_hack_10()
 print(hack_10)
--- hack_5.py
+++ hack_5.py
@@ -4,8 +4,17 @@ text: "fooziman" output => "f00z1m@n"
 
 def fn_hack_5():
     result = "fooziman"
-    result = "f00z1m@n"
-    return result
+    new_result = ""
+    for txt in result:
+        if (txt == 'o'):
+            new_result += '0'
+        elif (txt == 'i'):
+            new_result += '1'
+        elif (txt == 'a'):
+            new_result+= '@'
+        else:
+            new_result += txt
+    return new_result
 
 hack_5 = fn_hack_5()
 print(hack_5)
